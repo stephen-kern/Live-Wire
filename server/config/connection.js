05f8b80924c5074e99731e69fd61f70e9eaabf17
Vincent Toon <vincenttoon22@gmail.com>
2022-12-12 22:46:15 -0600

    starter server, connection, auth, dateFormat added

---
+++ server/config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/live-wire', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
