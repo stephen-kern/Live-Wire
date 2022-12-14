5071e06d7aefcefa42cfe9325e725a73c917da3e
Scott Rinaberger <scottrinaberger@gmail.com>
2022-12-14 09:28:48 -0600

    corrected savedReview in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveReview(input: reviewInput): User
+    savedReview(input: reviewInput): User
     removeReview(reviewId: String!): User
   }
 `;
