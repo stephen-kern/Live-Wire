4c5544f562a9941915a209324bc8aeb2d3c1d04b
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-12-13 18:18:22 -0600

    added bootstrap and started the header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,22 @@
+// ===IMPORTS===
+import React from "react";
+import { Link } from "react-router-dom";
+// import Auth from "../../utils/auth";
+
+const Header = () => {
+    // const logout = (event) => {
+    //     event.preventDefault();
+    //     Auth.logout();
+    // }
+
+    return (
+        <header>
+            <div>
+                
+            </div>
+        </header>
+    )
+
+}
+
+export default Header;
