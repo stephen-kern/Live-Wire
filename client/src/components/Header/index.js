0fe891046d93e6e3e5b605aa94c6144fbec16504
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-12-13 18:50:41 -0600

    finished header for now

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,24 +1,39 @@
 // ===IMPORTS===
 import React from "react";
 import { Link } from "react-router-dom";
-// import Auth from "../../utils/auth";
+import Auth from "../../utils/auth";
 
 const Header = () => {
-    // const logout = (event) => {
-    //     event.preventDefault();
-    //     Auth.logout();
-    // }
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
 
-    return (
-        <header>
-            <div>
-                <Link to="/">
-                    <h1> Live Wire </h1>
-                </Link>
-            </div>
-        </header>
-    )
+  return (
+    <header className="mb-4 py-2 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link to="/">
+          <h1 className="navbar-brand mb-0 h1"> Live Wire </h1>
+        </Link>
 
-}
+        <nav className="text-center">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
