83af6b78001faa0ec9ae679298ad6718b7a142e3
stephen <94320530+stephen-kern@users.noreply.github.com>
2022-12-14 15:04:27 -0600

    added some style to the header

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// === Package Imports ===
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+// === Component Imports === 
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+    <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
+    </ChakraProvider>
   );
 }
 
