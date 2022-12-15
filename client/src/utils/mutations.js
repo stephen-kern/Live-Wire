96bea5448c47dd431b827a117ec61af9f9569b5a
yaya <mamadou1991@outlook.com>
2022-12-14 20:03:48 -0600

    adding mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REVIEW = gql`
+  mutation addReview($input: reviewInput!) {
+    addOrder(input: $input) {
+      reviewText
+      createdAt
+      username
+      location
+      artist
+      reactions{
+        _id
+        reactionBody
+        username
+        createdAt
+     }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
