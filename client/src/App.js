cf1619c0bfb67adbc1496599e403016fdc2bdc6c
Vincent Toon <vincenttoon22@gmail.com>
2022-12-15 00:24:32 -0600

    login/signup functions added, now broken

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 // === Component Imports === 
 import Header from "./components/Header";
+// === Page Imports ===
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -11,6 +14,18 @@ function App() {
     <Router>
       <div>
         <Header />
+          <div className="container">
+            <Routes>
+              <Route
+              path="login"
+              element={<Login />} 
+              />
+              <Route
+              path="signup"
+              element={<Signup />}
+              />
+            </Routes>
+          </div>
       </div>
     </Router>
     </ChakraProvider>
