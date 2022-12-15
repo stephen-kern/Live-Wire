cd85808ca771b119e91df445a7dc28dc7f0c1200
Vincent Toon <vincenttoon22@gmail.com>
2022-12-14 22:08:55 -0600

    adding mama's code to develop

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,51 @@
-// ADD_REVIEW
-// ADD_COMMENT
-// ADD_BANDMATE
-// ADD_USER
-// LOGIN_USER
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REVIEW = gql`
+  mutation addReview($input: reviewInput!) {
+    addReview(input: $input) {
+      reviewText
+      createdAt
+      username
+      location
+      artist
+      reactions{
+        _id
+        reactionBody
+        username
+        createdAt
+     }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
