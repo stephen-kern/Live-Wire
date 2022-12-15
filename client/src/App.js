ffc70502ecfa2ed7ca5c513c286f67910192a526
Vincent Toon <vincenttoon22@gmail.com>
2022-12-15 00:36:02 -0600

    apollo provider added to app, signup works

--- client/src/App.js
+++ client/src/App.js
@@ -2,33 +2,56 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-// === Component Imports === 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+// === Component Imports ===
 import Header from "./components/Header";
 // === Page Imports ===
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <ChakraProvider>
-    <Router>
-      <div>
-        <Header />
-          <div className="container">
-            <Routes>
-              <Route
-              path="login"
-              element={<Login />} 
-              />
-              <Route
-              path="signup"
-              element={<Signup />}
-              />
-            </Routes>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Router>
+          <div>
+            <Header />
+            <div className="container">
+              <Routes>
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+              </Routes>
+            </div>
           </div>
-      </div>
-    </Router>
-    </ChakraProvider>
+        </Router>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
