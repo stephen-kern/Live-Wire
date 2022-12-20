e9bac372cd736c60357295744359b6f5902e7344
Vincent Toon <vincenttoon22@gmail.com>
2022-12-19 20:23:14 -0600

    deploying 1.2

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Live-Wire', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/live-wire', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
