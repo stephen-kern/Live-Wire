572f89b4ae0424f73d5163cc1d0f0fa1ac185cec
yaya <mamadou1991@outlook.com>
2022-12-21 16:45:57 -0600

    fixing merge

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -20,7 +20,6 @@ const HeaderComponent = () => {
   // JSX for global App that changes if a user is logged in
   return (
     <>
-<<<<<<< HEAD
         <Header className="nav">
           <div>
             <Link to="/mission">
@@ -55,42 +54,6 @@ const HeaderComponent = () => {
             )}
           </div>
         </Header>
-=======
-      <Header className="nav">
-        <div>
-          <Link to="/mission">
-            <h1 className="logo"> Live Wire </h1>
-          </Link>
-        </div>
-        <div className="nav-links-container">
-          {Auth.loggedIn() ? (
-            <>
-              <Link to="/" className="nav-links">
-                Setlist
-              </Link>
-              <Link to="/compose" className="nav-links">
-                Compose
-              </Link>
-              <Link to="/profile" className="nav-links">
-                Profile
-              </Link>
-              <a href="/" className="nav-links logout-btn" onClick={logout}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <>
-              <Link to="/login" className="nav-links">
-                Login
-              </Link>
-              <Link to="/signup" className="nav-links alt-btn">
-                Signup
-              </Link>
-            </>
-          )}
-        </div>
-      </Header>
->>>>>>> 55004519073eae7dea071e20d6b5acbeafc4edf2
     </>
   );
 };
